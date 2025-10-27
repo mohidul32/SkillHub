e1b89091d2cebf77aa33216d659a84b0bac77d10
mohidul <exit.mohidulislam@gmail.com>
2025-10-28 00:32:49 +0600

    Customize admin panel for Category, Course, and Lesson with search, filters, inline lessons, and student count

--- courses/admin.py
+++ courses/admin.py
@@ -3,17 +3,36 @@ from .models import Category, Course, Lesson
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
+    list_display = ('name', 'slug', 'description')
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 1  # show 1 empty row by default
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'instructor', 'category', 'is_published', 'price')
+    list_display = ('title', 'instructor', 'category', 'is_published', 'price', 'num_students')
     list_filter = ('category', 'is_published')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [LessonInline]
+
+    def num_students(self, obj):
+        return obj.students.count()
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(instructor=request.user)
+
+    num_students.short_description = 'Enrolled Students'
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     list_display = ('title', 'course', 'order')
     list_filter = ('course',)
+    search_fields = ('title', 'content')
+    ordering = ('course', 'order')
