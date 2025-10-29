1869d534337fb4c9f0e3388c46be0dd6f061dac4
mohidul <exit.mohidulislam@gmail.com>
2025-10-29 23:21:41 +0600

    Add password reset functionality using Django's built-in auth views

--- skillhub/settings.py
+++ skillhub/settings.py
@@ -140,3 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
+
+# Email Backend (for development)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# For production, you can use SMTP settings instead
