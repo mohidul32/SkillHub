cbe442fc1cd42d1149cf93ccd789d965736184f2
mohidul <exit.mohidulislam@gmail.com>
2025-10-29 23:49:20 +0600

    Add email verification system for user signup

--- users/views.py
+++ users/views.py
@@ -3,17 +3,60 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import UserSignupForm, UserLoginForm, StudentSignUpForm, InstructorSignUpForm
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.contrib import messages
+
+User = get_user_model()
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserSignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('dashboard')
+            user = form.save(commit=False)
+            user.is_active = False  # deactivate until email verification
+            user.save()
+
+            send_activation_email(request, user)
+            messages.success(request, "Please check your email to activate your account.")
+            return redirect('login')
     else:
         form = UserSignupForm()
     return render(request, 'users/signup.html', {'form': form})
+
+def send_activation_email(request, user):
+    current_site = get_current_site(request)
+    subject = "Activate your account"
+    message = render_to_string('users/account_activation_email.html', {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
+    })
+    email = EmailMessage(subject, message, to=[user.email])
+    email.send()
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Your account has been activated! You can now log in.")
+        return redirect('login')
+    else:
+        return render(request, 'users/activation_invalid.html')
+
 
 def login_view(request):
     if request.method == 'POST':
